7c678147f24b00f20aef4443fb066bd34b257226
KolovraTT_ <165069396+Kol0vratT@users.noreply.github.com>
2026-02-16 18:32:50 +0400

    Update LayerMask.hpp

--- Unity/API/LayerMask.hpp
+++ Unity/API/LayerMask.hpp
@@ -26,12 +26,22 @@ namespace Unity
 
 		inline System_String* LayerToName(unsigned int m_uLayer)
 		{
+			if (!m_LayerMaskFunctions.m_LayerToName)
+				return nullptr;
+
 			return reinterpret_cast<System_String * (UNITY_CALLING_CONVENTION)(unsigned int)>(m_LayerMaskFunctions.m_LayerToName)(m_uLayer);
 		}
 
 		inline uint32_t NameToLayer(const char* m_pName)
 		{
-			return reinterpret_cast<uint32_t(UNITY_CALLING_CONVENTION)(void*)>(m_LayerMaskFunctions.m_NameToLayer)(IL2CPP::String::New(m_pName));
+			if (!m_LayerMaskFunctions.m_NameToLayer || !m_pName)
+				return static_cast<uint32_t>(-1);
+
+			System_String* name = IL2CPP::String::New(m_pName);
+			if (!name)
+				return static_cast<uint32_t>(-1);
+
+			return reinterpret_cast<uint32_t(UNITY_CALLING_CONVENTION)(void*)>(m_LayerMaskFunctions.m_NameToLayer)(name);
 		}
 	}
 }
