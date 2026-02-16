540eeb29b8cd6f8024ddbdcd56addb9ee5b817ac
KolovraTT_ <165069396+Kol0vratT@users.noreply.github.com>
2026-02-16 18:24:44 +0400

    Update Domain.hpp

--- API/Domain.hpp
+++ API/Domain.hpp
@@ -6,12 +6,22 @@ namespace IL2CPP
 	{
 		void* Get()
 		{
+			if (!Functions.m_DomainGet)
+				return nullptr;
+
 			return reinterpret_cast<void*(IL2CPP_CALLING_CONVENTION)()>(Functions.m_DomainGet)();
 		}
 
 		Unity::il2cppAssembly** GetAssemblies(size_t* m_Size)
 		{
-			return reinterpret_cast<Unity::il2cppAssembly**(IL2CPP_CALLING_CONVENTION)(void*, size_t*)>(Functions.m_DomainGetAssemblies)(Get(), m_Size);
+			if (!Functions.m_DomainGetAssemblies)
+				return nullptr;
+
+			void* m_Domain = Get();
+			if (!m_Domain)
+				return nullptr;
+
+			return reinterpret_cast<Unity::il2cppAssembly**(IL2CPP_CALLING_CONVENTION)(void*, size_t*)>(Functions.m_DomainGetAssemblies)(m_Domain, m_Size);
 		}
 	}
 }
